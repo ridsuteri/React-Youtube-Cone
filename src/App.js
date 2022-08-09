0762c054882a980a59cb2a46503971e557840f9f
Riddhi Suteri <ridsuteri@gmail.com>
2022-08-09 20:29:54 +0530

    add Recommended and VideoCard component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Recommended from './components/Recommended/Recommended';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
               <Header />
               <div className="app__mainpage">
                 <Sidebar />
+                <Recommended/>
               </div>
             </>
           } />
